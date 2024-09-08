bac6bcedb5065c6a881726028350036bd224449e
Dhruv Singla <singladhruv18@gmail.com>
2024-09-08 12:31:26 +0530

    logs implemented

--- lib/generateApiKey.ts
+++ lib/generateApiKey.ts
@@ -1,7 +1,7 @@
 // utils/generateApiKey.ts
 
 export function generateApiKey(length = 32) {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-=@#%&*"
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-@#%&*"
     let apiKey = ""
   
     for (let i = 0; i < length; i++) {
